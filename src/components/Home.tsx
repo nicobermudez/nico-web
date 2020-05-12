896bb0066be77ab076b01c22084b21af930a8654
Nicolas Bermudez <nicolas.bermudez@ef.com>
2020-05-12 17:20:29 -0400

    loading

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,6 +1,6 @@
 import styled from "@emotion/styled";
-import React, { useState } from "react";
-import { Enter } from "./Enter";
+import React, { useState, useEffect } from "react";
+import { Loading } from "./Loading";
 import { Intro } from "./Intro";
 import { Scroll } from "./Scroll";
 
@@ -17,15 +17,23 @@ const HomeContainer = styled.div({
 
 export const Home: React.FC = () => {
     const [enter, setEnter] = useState(false);
+    const [loading, setLoading] = useState(true);
+
+    useEffect(() => {
+        const timer = setTimeout(() => {
+            setLoading(false);
+          }, 2000);
+          return () => clearTimeout(timer);
+    }, []);
 
     return (
         <HomeContainer>
-            {enter 
-                ? <div>
+            {loading 
+                ? <Loading />
+                : <div>
                     <Intro />
                     <Scroll />
                 </div>
-                : <Enter enter={enter} setEnter={setEnter} />
             }
         </HomeContainer>
     );
