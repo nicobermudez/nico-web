5d46aae6c6753858cdf2f6c7cc972726e3aacc12
Nico Bermudez <bermnicolas@gmail.com>
2021-11-20 20:50:22 -0500

    uncomment

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,10 +1,11 @@
 import styled from "@emotion/styled";
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { About } from "./About";
 import { Email } from "./Email";
 import { Links } from "./Links";
 import { Intro } from "./Intro";
 import { Tip } from "./Tip";
+import { Loading } from "./Loading";
 
 const Wrapper = styled.div({
     fontFamily: "sans-serif",
@@ -12,14 +13,18 @@ const Wrapper = styled.div({
 });
 
 export const Home: React.FC = () => {
-    // const [loading, setLoading] = useState(true);
+    const [loading, setLoading] = useState(true);
 
-    // useEffect(() => {
-    //     const timer = setTimeout(() => {
-    //         setLoading(false);
-    //     }, 2000);
-    //     return () => clearTimeout(timer);
-    // }, []);
+    useEffect(() => {
+        const timer = setTimeout(() => {
+            setLoading(false);
+        }, 2000);
+        return () => clearTimeout(timer);
+    }, []);
+
+    if (loading) {
+        return <Loading />;
+    }
 
     return (
         <>
