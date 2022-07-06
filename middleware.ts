5d079abd6b9b7a99c5321c3a676efdccac703e77
nicobermudez <nicolas.bermudez@ef.com>
2022-07-06 16:14:20 +0200

    test

--- middleware.ts
+++ middleware.ts
@@ -1,26 +1,44 @@
-import type { NextRequest } from "next/server";
+import { NextRequest, NextResponse } from "next/server";
+import { countries } from "@app/lib";
 
-import { NextResponse } from "next/server";
+export type UserGeolocationData = {
+    country?: string;
+    city?: string;
+    region?: string;
+    currencyCode?: string;
+    currencySymbol?: string;
+    name?: string;
+    languages?: string;
+    greeting?: string;
+};
 
-// Block Austria, prefer Germany
-const BLOCKED_COUNTRY = "AT";
-
-// Limit middleware pathname config
+// run only on homepage
 export const config = {
     matcher: "/",
 };
 
-export function middleware(req: NextRequest) {
-    // Extract country
-    const country = req.geo.country || "US";
+export function middleware(req: NextRequest): NextResponse {
+    console.log("In the middle");
+    const { nextUrl: url, geo } = req;
+    const country = geo.country || "US";
+    const city = geo.city || "Boston";
+    const region = geo.region || "MA";
 
-    // Specify the correct pathname
-    if (country === BLOCKED_COUNTRY) {
-        req.nextUrl.pathname = "/blocked";
-    } else {
-        req.nextUrl.pathname = `/${country}`;
-    }
+    const countryInfo = countries.find((x) => x.cca2 === country);
 
-    // Rewrite to URL
-    return NextResponse.rewrite(req.nextUrl);
+    const currencyCode = Object.keys(countryInfo.currencies)?.[0];
+    const currency = countryInfo.currencies?.[currencyCode];
+    const languages = Object.values(countryInfo.languages).join(", ");
+    const greeting = countryInfo.greeting ?? "Hello";
+
+    url.searchParams.set("country", country);
+    url.searchParams.set("city", city);
+    url.searchParams.set("region", region);
+    url.searchParams.set("currencyCode", currencyCode);
+    url.searchParams.set("currencySymbol", currency.symbol);
+    url.searchParams.set("name", currency.name);
+    url.searchParams.set("languages", languages);
+    url.searchParams.set("greeting", greeting);
+
+    return NextResponse.rewrite(url);
 }
