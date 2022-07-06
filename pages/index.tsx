5e6be8bb483d89cf474503b58036588661f99339
nicobermudez <nicolas.bermudez@ef.com>
2022-07-06 16:55:29 +0200

    props

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,15 @@
 import { About, Intro, Links, Tip } from "@app/components";
 import { Email } from "@app/components";
+import { UserGeolocationData } from "@app/middleware";
 
-export default function Index() {
+export const getServerSideProps = ({ query }: any) => ({
+    props: query,
+});
+
+export default function Index({ greeting }: UserGeolocationData) {
     return (
         <>
-            <Intro />
+            <Intro greeting={greeting} />
             <About />
             <Links />
             <Email />
